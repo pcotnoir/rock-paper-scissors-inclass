1be7b732a7f1d96bdf7a945fb2f4ef195a198393
pcotnoir <pc2508@stern.nyu.edu>
2019-06-05 19:46:24 -0400

    Determining the winning option

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ print("USER CHOSE:",user_choice)
 # VALIDATE INPUTS
 
 options = ["rock","paper","scissors"]
+
 if user_choice in options: #in operator looks for a value or word in a list
     pass #or use print("VALID") to show VALID when rock, paper, scissors is correctly inputted.
 else:
@@ -21,13 +22,27 @@ else:
 
 # GENERATE COMPUTER SELECTION
 
-computer_choice = random.choice(["rock","paper","scissors"])
+computer_choice = random.choice(options)
 print("------------")
 print("GENERATING...")
 print("COMPUTER CHOSE:",computer_choice)
 
 # DETERMINE THE WINNER
+## rock beats scissors, paper beats rock, scissors beats paper, same selection is a tie
 
-
+if user_choice == computer_choice:
+    print("TIE")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("ROCK")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("PAPER")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("ROCK")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
 
 # DISPLAY FINAL OUTPUTS / OUTCOMES
