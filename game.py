10a7764f993935cf1876278709b9f9944f17dfa9
pcotnoir <pc2508@stern.nyu.edu>
2019-06-05 19:26:32 -0400

    Generate computer choice

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 # game.py
 
+import random #always include imports of modules at the start of a Python script. Prevents error of computer selection.
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # CAPTURE INPUTS
@@ -11,17 +13,20 @@ print("USER CHOSE:",user_choice)
 # VALIDATE INPUTS
 
 if user_choice in ["rock","paper","scissors"]: #in operator looks for a value or word in a list
-    print("VALID")
+    pass #or use print("VALID") to show VALID when rock, paper, scissors is correctly inputted.
 else:
         print("INVALID SELECTION, PLEASE TRY AGAIN...")
         exit()
 
 # GENERATE COMPUTER SELECTION
 
+computer_choice = random.choice(["rock","paper","scissors"])
+print("------------")
 print("GENERATING...")
-
+print("COMPUTER CHOSE:",computer_choice)
 
 # DETERMINE THE WINNER
+
 
 
 # DISPLAY FINAL OUTPUTS / OUTCOMES
